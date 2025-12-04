8283ae163e955ea6514ac5aa9dd630a49316e297
git-rohini <official.rohinisharma23@gmail.com>
2025-12-04 20:55:40 +0000

    the code

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+// variables declare
+/*const accountId =144553
+let accountEmail ="rohini@google.com"
+var accountPassword="12345"
+accountCity = "jaipur"
+let accountState;*/
+
+// 1)proof: for "const variable"-this variable cannot be changed for example; the user id should remain fixed
+
+//console.log(accountId);   ----- this will work
+
+//  //it will print the accountId but if any changes made in code doesn't mean change in the output
+// **********************************************************************************************************************************
+//for example let say :we are decalring the value given below will not be executed, we can write code here but this will show error
+//accountId=2  -----not allowed 
+//console.log(accountId); ---- this won't work 
+
+//2) proof:
+/*accountEmail="rs2rs.com"
+accountPassword="323232"
+accountCity="bengaluru"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])*/
+
+// NOTE: Prefer not to use 'var' because of issue in block scope and functional scope //
+
+
